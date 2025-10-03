b0a876254d3353a234b3f145734a705a19386dc2
Albin Smajli <64081932+AlbinSmajli22@users.noreply.github.com>
2025-10-03 03:01:49 +0200

    .
    .

--- user-task/src/App.js
+++ user-task/src/App.js
@@ -1,25 +1,48 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users') 
-      .then(response => response.json())
-      .then(data => setUsers(data));
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setUsers(data));
   }, []);
+
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(search.toLowerCase()) ||
+      user.email.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="App">
-      <h1>User List</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>
-            {user.name} ({user.email})
-          </li>
+      <h1 className="title">Users</h1>
+      <input
+        type="text"
+        placeholder="Search users..."
+        className="search-input"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="card-container">
+        {filteredUsers.map((user) => (
+          <div className="card" key={user.id}>
+            <img src="user.png" className="card-img-top" alt="User Avatar" />
+            <div className="card-body">
+              <h5 className="card-title">Name: {user.name}</h5>
+              <p className="card-text">E-mail: {user.email}</p>
+              <p className="card-text">Company: {user.company.name}</p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
